7c68b71a542ff6a26f8b39b4e172db6a6031572a
Gary Larson <gary.larson@outlook.com>
2021-06-30 09:31:03 -0500

    Completed project

--- covid/cli.py
+++ covid/cli.py
@@ -1,16 +1,23 @@
 ''' Module to run the command line interface '''
-from data.setup import setup_mongo
-from data.covid_dao import MongoDao
-from data.mongo_utility import get_connection
 from user.model import User
 
+from data.setup import setup_mongo
+from data.mongo_utility import get_connection
+from data.covid_dao import get_user_by_name
+from data.covid_dao import add_user
+from data.covid_dao import remove_user_by_id
+from data.covid_dao import get_states
+from data.covid_dao import get_percents
+from data.covid_dao import update_user_by_id
+
+# pylint: disable=invalid-name
 conn = get_connection()
 db = conn.get_database('pycovid')
 states_col = db.get_collection("states")
 users_col = db.get_collection("users")
 counters_col = db.get_collection("counters")
-# Get DAO
-dao = MongoDao()
+LIMIT = 10
+STATE_COUNT = states_col.count_documents({})
 def print_main_menu(name):
     ''' Method to print the main menu '''
     print("\nWelcome " + name)
@@ -28,119 +35,171 @@ def print_user_menu():
 def input_name():
     ''' Method to get the user name from the user '''
     is_invalid_name = True
-    name = ''
     while is_invalid_name:
-        print("Enter a name (letters and numbers only)")
+        print("\n\nEnter a name (letters and numbers only)")
         print("Press 'q' or 'Q' for previous menu")
-        user_input = input("Enter Choice: ")
-        if user_input.isalnum():
+        my_input = input("Enter Choice: ")
+        if my_input.isalnum():
             is_invalid_name = False
         else:
             print("Invalid entry please try again!")
-    return user_input
+    return my_input
 def get_user():
     ''' Method to get the user from the database '''
     name_exists = False
     not_quit = True
     name = ''
-    user = User()
+    my_user = User()
     while not name_exists and not_quit:
         name = input_name()
-        if name == 'q' or name == 'Q':
+        if name in ('q', 'Q'):
             not_quit = False
         else:
-            dict_user = dao.get_user_by_name(users_col, name)
-            if dict_user == None:
+            dict_user = get_user_by_name(users_col, name)
+            if dict_user is None:
                 print(name + " does not exist! Please choose another.")
             else:
                 name_exists = True
-                user.set_id(dict_user['_id'])
-                user.name = dict_user['name']
-                user.last_visited = dict_user['last_visited']
-                user.state_offset = dict_user['state_offset']
-                user.percent_offset = dict_user['percent_offset']
-    return user
+                my_user.db_id = dict_user['_id']
+                my_user.name = dict_user['name']
+                my_user.last_visited = dict_user['last_visited']
+                my_user.state_offset = dict_user['state_offset']
+                my_user.percent_offset = dict_user['percent_offset']
+    return my_user
 def create_user():
     ''' Method to create a user in the database '''
     name_exists = True
     not_quit = True
     name = ''
-    user = User()
+    my_user = User()
     while name_exists and not_quit:
         name = input_name()
-        if name == 'q' or name == 'Q':
+        if name in ('q', 'Q'):
             not_quit = False
         else:
-            dict_user = dao.get_user_by_name(users_col, name)
-            if dict_user == None:
+            dict_user = get_user_by_name(users_col, name)
+            if dict_user is None:
                 name_exists = False
-                user.name = name
-                user.state_offset = 0
-                user.percent_offset = 0
-                dao.add_user(users_col, counters_col, user)
-                dict_user = dao.get_user_by_name(users_col, name)
-                user.set_id(dict_user['_id'])
-                user.name = dict_user['name']
-                user.last_visited = dict_user['last_visited']
-                user.state_offset = dict_user['state_offset']
-                user.percent_offset = dict_user['percent_offset']
+                my_user.name = name
+                my_user.state_offset = 0
+                my_user.percent_offset = 0
+                add_user(users_col, counters_col, user)
+                dict_user = get_user_by_name(users_col, name)
+                my_user.db_id = dict_user['_id']
+                my_user.name = dict_user['name']
+                my_user.last_visited = dict_user['last_visited']
+                my_user.state_offset = dict_user['state_offset']
+                my_user.percent_offset = dict_user['percent_offset']
             else:
                 print(name + " Exists Please choose another name!")
-    return user 
+    return my_user
 def delete_user():
     ''' Method to delete the user from the database '''
     name_exists = False
     not_quit = True
     name = ''
-    user = User()
     while not name_exists and not_quit:
         name = input_name()
-        if name == 'q' or name == 'Q':
+        if name in ('q', 'Q'):
             not_quit = False
         else:
-            dict_user = dao.get_user_by_name(users_col, name)
-            if dict_user == None:
+            dict_user = get_user_by_name(users_col, name)
+            if dict_user is None:
                 print(name + " exists! Please choose another.")
             else:
                 name_exists = True
                 confirm_delete = False
                 while not confirm_delete:
                     print("are you sure you want to delete user: " + name)
-                    user_input = input("Press 'y', 'Y', 'n', or 'N': ")
-                    if user_input in ['y', 'Y']:
-                        dao.remove_user_by_id(users_col, dict_user['_id'])
+                    my_user_input = input("Press 'y', 'Y', 'n', or 'N': ")
+                    if my_user_input in ['y', 'Y']:
+                        remove_user_by_id(users_col, dict_user['_id'])
                         confirm_delete = True
                         name_exists = True
-                    elif user_input in ['n', 'N']:
+                    elif my_user_input in ['n', 'N']:
                         confirm_delete = True
                         name_exists = True
                     else:
                         print("Invalid Entry Please try again")
-    return None            
 def get_user_name():
     ''' Method to get the user name '''
     keep_looping = True
     while keep_looping:
         print_user_menu()
-        user_input = input('Enter choice: ')
-        if user_input[0] == '1':
-            user = get_user()
-            if not user.name == '':
+        my_user_input = input('Enter choice: ')
+        if my_user_input[0] == '1':
+            my_user = get_user()
+            if my_user.name != '':
                 keep_looping = False
-        elif user_input[0] == '2':
-            user = create_user()
-            if not user.name == '':
+        elif my_user_input[0] == '2':
+            my_user = create_user()
+            if my_user.name != '':
                 keep_looping = False
-        elif user_input[0] == '3':
+        elif my_user_input[0] == '3':
             delete_user()
-        elif user_input[0] == 'q' or user_input[0] == 'Q':
+        elif my_user_input[0] == 'q' or user_input[0] == 'Q':
             keep_looping = False
+            my_user.name = my_user_input[0]
         else:
             print('Invalid entry please try again.')
-    return user
+    return my_user
 
 
 if __name__ == '__main__':
     setup_mongo()
     user = get_user_name()
-    print_main_menu(user.name)
+    continue_looping = True
+    if user.name == 'q' or user.name == 'Q':
+        continue_looping = False
+    while continue_looping:
+        print_main_menu(user.name)
+        user_input = input('Enter choice: ')
+        if user_input[0] == '1':
+            # Print screen with basic information and update user
+            state_offset = user.state_offset
+            states = get_states(states_col, state_offset, LIMIT)
+            if states is None:
+                print("No results")
+            else:
+                # Print header
+                print("               State  Population Population-Rank Total-Cases New-Cases" +
+                    " Total-Deaths New-Deaths Total-Active-Cases")
+                for state in states:
+                    print(format("%20s %11s %15s %11s %9s %12s %10s %18s" % (state['state'],
+                    "{:,}".format(state['population']), "{:,}".format(state['populationUSARank']),
+                    "{:,}".format(state['totalCases']), "{:,}".format(state['newCases']),
+                    "{:,}".format(state['totalDeaths']), "{:,}".format(state['newDeaths']),
+                    "{:,}".format(state['totalActiveCases']))))
+                # update user state offset
+                state_offset += LIMIT
+                if state_offset > STATE_COUNT:
+                    state_offset = 0
+                update_user_by_id(users_col, user.db_id, state_offset, user.percent_offset)
+        elif user_input[0] == '2':
+            # print screen with percent information and update user
+            percent_offset = user.percent_offset
+            states = get_percents(states_col, percent_offset, LIMIT)
+            if states is None:
+                print("No results")
+            else:
+                # Print header
+                print("               State Percent-Population Mortality-Rate Percent-Cases " +
+                    "Percent-Deaths Percent-Recovered Percent-Active-Cases")
+                for state in states:
+                    print("%20s %18s %14s %13s %14s %17s %20s" % (state['state'],
+                    "{:,.2f}".format(state['pcOfUSAPopulation']),
+                    "{:,.2f}".format(state['mortalityRate']),
+                    "{:,.2f}".format(state['pcOfUSATotalCases']),
+                    "{:,.2f}".format(state['pcOfUSADeaths']),
+                    "{:,.2f}".format(state['pcOfUSARecovered']),
+                    "{:,.2f}".format(state['pcOfUSAActiveCases'])))
+                # update user state offset
+                percent_offset += LIMIT
+                if percent_offset > STATE_COUNT:
+                    percent_offset = 0
+                update_user_by_id(users_col, user.db_id, user.state_offset, percent_offset)
+        elif user_input[0] == 'q' or user_input[0] == 'Q':
+            # Quit the program
+            continue_looping = False
+        else:
+            print('Invalid Entry Please try again!')
--- covid/user/model.py
+++ covid/user/model.py
@@ -4,7 +4,7 @@ class User:
     ''' A class that defines what user data is '''
     def __init__(self, **kwargs):
         ''' A method that initializes an object of this class '''
-        self._id = kwargs['db_id'] if 'db_id' in kwargs.keys() else 0
+        self.db_id = kwargs['db_id'] if 'db_id' in kwargs.keys() else 0
         self.name = kwargs['name'] if 'name' in kwargs.keys() else ''
         self.last_visited = kwargs['last_visited'] if 'last_visited' in kwargs.keys() else ''
         self.state_offset = kwargs['state_offset'] if 'state_offset' in kwargs.keys() else 0
@@ -19,9 +19,3 @@ class User:
     def to_dict(self):
         ''' Returns a dictionary representation of the object '''
         return self.__dict__
-    def set_id(self, _id):
-        ''' Method to set the id '''
-        self._id = _id
-    def set_last_visited(self, last_visited):
-        ''' Method to set the last visited date and time '''
-        self.last_visited = last_visited
